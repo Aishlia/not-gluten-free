753ad7a7885afbced4643a25b91fa7a396ed0d6f
Julia Nai <nai.julia@gmail.com>
2018-05-24 17:41:54 -0700

    can add new shape, added classes

--- main.js
+++ main.js
@@ -102,227 +102,168 @@ var boundingBox = {
   ]
 };
 
-// Converting from input shapes to usable entry for SAT shapes
-// Generates rectangle coordinates
-function generate_rectangle(shape, center) {
-  // console.log(shape)
-  x_adjust = shape.dimensions['w']/2;
-  y_adjust = shape.dimensions['h']/2;
-
-  // Adjust nodes to be defined from center of shape
-  for (i of shape.nodes) {
-    i['x'] += center.x
-    i['y'] += center.y
+class Shape_cluster {
+  constructor(shapeList){
+    this.shapeList = shapeList;
   }
 
-  var vertices = [
+  get shape_coords() {
+    return this.optomize()
+  }
+
+  add_new_shape(new_shape) {
+    for (var shape of this.shapeList) {
+      shape.pinned = true
+    }
+
+    this.shapeList.push(new_shape)
+    return this.optomize()
+  }
+
+  optomize() {
+    var shapedata = this.generate_shapedata(this.shapeList)
+    var sim = new Simulation();
+    var svg = d3.select("#display").append("svg")
+    .attr("width", window.innerWidth)
+    .attr("height", window.innerHeight)
+    .append("g")
+    .attr("transform", "translate(" + window.innerWidth / 2 + ", " +
+    window.innerHeight / 2 + ")");
+    var display = new ShapeSVG(shapedata, svg);
+
+    var output_coords = this.animate(shapedata, shapeList, sim, display)
+    return output_coords
+  }
+
+  // Converting from input shapes to usable entry for SAT shapes
+  // Generates rectangle coordinates
+  generate_rectangle(shape, center) {
+    // console.log(shape)
+    var x_adjust = shape.dimensions['w']/2;
+    var y_adjust = shape.dimensions['h']/2;
+
+    // Adjust nodes to be defined from center of shape
+    for (var i of shape.nodes) {
+      i['x'] += center.x
+      i['y'] += center.y
+    }
+
+    var vertices = [
       {x: (center.x - x_adjust), y: (center.y - y_adjust)},
       {x: (center.x + x_adjust),  y: (center.y - y_adjust)},
       {x: (center.x + x_adjust),  y: (center.y + y_adjust)},
       {x: (center.x - x_adjust),  y: (center.y + y_adjust)}
-  ]
+    ]
 
-  new_shape = {
+    var new_shape = {
       vertices: vertices,
       nodes: shape.nodes,
       pinned: shape.pinned,
       center: center
-  };
+    };
 
-  return new_shape;
-}
+    return new_shape;
+  }
 
-// Generates circle coordinates as n-sided polygonal approximation of circle
-function generate_circle(shape, center) {
-  var radius = shape.dimensions['r'];
-  var rotation = shape.rotation;
-  var pinned = shape.pinned;
-  var vertices = [];
+  // Generates circle coordinates as n-sided polygonal approximation of circle
+  generate_circle(shape, center) {
+    var radius = shape.dimensions['r'];
+    var rotation = shape.rotation;
+    var pinned = shape.pinned;
+    var vertices = [];
 
-  // Approximate the circle with a polygon
-  var sides = 30; // Number of sides for the polygon approximation
-  var theta = 0;
-  for (var i = 0; i < sides; i++) {
-    theta += (2*Math.PI)/sides;
-    vertices.push({x: (radius * Math.cos(theta)) + center.x,
-      y: radius * Math.sin(theta) + center.y})
+    // Approximate the circle with a polygon
+    var sides = 30; // Number of sides for the polygon approximation
+    var theta = 0;
+    for (var i = 0; i < sides; i++) {
+      theta += (2*Math.PI)/sides;
+      vertices.push({x: (radius * Math.cos(theta)) + center.x,
+        y: radius * Math.sin(theta) + center.y})
+      }
+
+      // Adjust nodes to be defined from center of shape
+      for (i of shape.nodes) {
+        i['x'] += center.x
+        i['y'] += center.y
+      }
+
+      var new_shape = {
+        vertices: vertices,
+        nodes: shape.nodes,
+        pinned: shape.pinned,
+        center: center
+      }
+
+      return new_shape;
     }
 
-  // Adjust nodes to be defined from center of shape
-  for (i of shape.nodes) {
-    i['x'] += center.x
-    i['y'] += center.y
+  // Converts a single shape from user input to SAT input
+  convert_shape(shape) {
+      var converted_shape; // Shape in usable format
+
+      if (typeof shape.coordinates === 'undefined') {
+        shape.coordinates = { x: 0, y: 0};
+      }
+
+      // Centroid of shape
+      var center = {
+        x: shape.coordinates.x,
+        y: shape.coordinates.y
+      }
+
+      if (shape.dimensions['h'] && shape.dimensions['w']) // Rectangle definition
+      converted_shape = this.generate_rectangle(shape, center);
+      else if (shape.dimensions['r']) // Circle definition
+      converted_shape = this.generate_circle(shape, center);
+
+      return converted_shape;
+    }
+
+  // Converts the user defined shapelist to SAT usable coordinates
+  convert_shape_list(shapeList, boundingBox) {
+      var converted_shape_list = [];
+      var converted_shape;
+
+      for (var shape of shapeList) {
+        converted_shape = this.convert_shape(shape)
+        converted_shape_list.push(converted_shape);
+      }
+
+      return converted_shape_list
+    }
+
+  // Generates SAT shapedata
+  generate_shapedata(shapeList, boundingBox) {
+      var shapes = this.convert_shape_list(shapeList, boundingBox);
+
+      return (new ShapeData(shapes)) //shapedata
+    }
+
+  generate_output_coords(shapedata, shapeList) {
+    var output_coords = [];
+
+    for (var s in shapedata.shapes) {
+      var new_shape = shapeList[s]
+      new_shape.coordinates = {x: shapedata.shapes[s].pos.x, y: shapedata.shapes[s].pos.y}
+      new_shape.rotation = shapedata.shapes[s].angle * 180 / Math.PI
+      output_coords.push(new_shape)
+    }
+
+    return output_coords
   }
 
-  new_shape = {
-    vertices: vertices,
-    nodes: shape.nodes,
-    pinned: shape.pinned,
-    center: center
-  }
+  animate(shapedata, shapeList, sim, display) {
+      for (var i = 0; i < 1000; i++) {
+        sim.step(shapedata);
+      }
+      var result = this.generate_output_coords(shapedata, shapeList);
+      // write_cost(shapedata); // d3
+      display.rerender(); // d3
 
-  return new_shape;
+      return result;
+    }
+
 }
 
-// Converts a single shape from user input to SAT input
-function convert_shape(shape) {
-  var converted_shape; // Shape in usable format
-
-  if (typeof shape.coordinates === 'undefined') {
-    shape.coordinates = { x: 0, y: 0};
-  }
-
-  // Centroid of shape
-  var center = {
-    x: shape.coordinates.x,
-    y: shape.coordinates.y
-  }
-
-  if (shape.dimensions['h'] && shape.dimensions['w']) // Rectangle definition
-  converted_shape = generate_rectangle(shape, center);
-  else if (shape.dimensions['r']) // Circle definition
-  converted_shape = generate_circle(shape, center);
-
-  return converted_shape;
-}
-
-// Creates shapes for the boundig box NOTE: does not work
-function make_bounding_box(boudingBox){
-  i = boundingBox.vertices
-  thick = 100
-  length = 2 * Math.abs(i[1].x - i[0].x)
-  return [{
-    dimensions: { h: thick, w: length}, // most likely assume in mm (1mm = 3.779528px)
-    coordinates: { x: i[1].x, y: i[1].y-thick/2}, // in px also may be undefined (when initializing)
-    rotation: 0, // in degrees
-    pinned: true,
-    nodes: [{x: 0, y: 20, color: 4}]
-  },
-  {
-    dimensions: { h: thick, w: length}, // most likely assume in mm (1mm = 3.779528px)
-    coordinates: { x: i[2].x+thick/2, y: i[2].y}, // in px also may be undefined (when initializing)
-    rotation: 90, // in degrees
-    pinned: true,
-    nodes: [{x: 0, y: 20, color: 4}]
-  },
-  {
-    dimensions: { h: thick, w: length}, // most likely assume in mm (1mm = 3.779528px)
-    coordinates: { x: i[3].x, y: i[3].y+thick/2}, // in px also may be undefined (when initializing)
-    rotation: 180, // in degrees
-    pinned: true,
-    nodes: [{x: 0, y: 20, color: 4}]
-  },
-  {
-    dimensions: { h: thick, w: length}, // most likely assume in mm (1mm = 3.779528px)
-    coordinates: { x: i[0].x-thick/2, y: i[0].y}, // in px also may be undefined (when initializing)
-    rotation: 270, // in degrees
-    pinned: true,
-    nodes: [{x: 0, y: 20, color: 4}]
-  }]
-}
-
-// Converts the user defined shapelist to SAT usable coordinates
-function convert_shape_list(shapeList, boundingBox) {
-  var converted_shape_list = [];
-  var converted_shape;
-
-  for (shape of shapeList) {
-    converted_shape = convert_shape(shape)
-    converted_shape_list.push(converted_shape);
-  }
-
-  return converted_shape_list
-}
-
-// Generates SAT shapedata
-function generate_shapedata(shapeList, boundingBox) {
-  var shapes = convert_shape_list(shapeList, boundingBox);
-  // bounding_box = convert_shape_list(make_bounding_box(boundingBox))
-  // for (i of bounding_box) {
-  //   shapes.push(i)
-  // }
-
-  // var shapedata = new ShapeData(shapes);
-  return (new ShapeData(shapes)) //shapedata
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-var iters = 0,
-    threshold = 0,
-    max_iters = 1000,
-    done = false;
-
-function iterate_sim(shapedata, sim) {
-  // Early termination or recursion
-  iters += 1;
-  threshold = 0; // maximum component of momentum
-
-  sim.step(shapedata);
-
-  // Threshold is never use. Program always ends on max_ites.
-
-  // shapedata.shapes.forEach(function(n) {
-  //   if (!n.pinned) threshold = Math.max(threshold, Math.abs(n.lin_p.x));
-  //   if (!n.pinned) threshold = Math.max(threshold, Math.abs(n.lin_p.y));
-  //   if (!n.pinned) threshold = Math.max(threshold, Math.abs(n.rot_p));
-  // });
-  // return (threshold < 10 || iters > max_iters);
-  return(iters > max_iters);
-}
-
-function generate_output_coords(shapedata, shapeList) {
-  output_coords = [];
-
-  for (s in shapedata.shapes) {
-    new_shape = shapeList[s]
-    new_shape.coordinates = {x: shapedata.shapes[s].pos.x, y: shapedata.shapes[s].pos.y}
-    new_shape.rotation = shapedata.shapes[s].angle * 180 / Math.PI
-    output_coords.push(new_shape)
-  }
-
-  return output_coords
-}
-
-function animate(shapedata, shapeList) {
-  done = iterate_sim(shapedata, sim);
-
-  if (done) {
-    result = generate_output_coords(shapedata, shapeList);
-    write_cost(shapedata); // d3
-    display.rerender(); // d3
-  } else {
-    animate(shapedata, shapeList);
-  }
-
-  return result;
-}
-
-// ////////////////////////////////////////////////////////////////////////////////
-var sim = new Simulation();
-
-var shapedata = generate_shapedata(shapeList)
-
-function main(boudingBox) {
-  output_coords = animate(shapedata, shapeList)
-
-  return output_coords
-}
-
-// d3 start
-var svg = d3.select("#display").append("svg")
-    .attr("width", window.innerWidth)
-    .attr("height", window.innerHeight)
-  .append("g")
-    .attr("transform", "translate(" + window.innerWidth / 2 + ", " +
-                                      window.innerHeight / 2 + ")");
-
-var display = new ShapeSVG(shapedata, svg);
-
-function write_cost(shapedata) {
-    d3.select("#cost").text(shapedata.get_cost().toPrecision(3));
-}
-// stop
-
-output_coords = main(boundingBox)
-console.log(output_coords)
+board = new Shape_cluster(shapeList)
+console.log(board.shape_coords)
--- shape-physics.js
+++ shape-physics.js
@@ -32,218 +32,208 @@ function Simulation() {
 };
 
 Simulation.prototype.step = function(shapedata) {
-  // console.log(shapedata)
-  if (typeof shapedata.grouped_nodes[0] === 'undefined') { // Find out why there is an empty
-    shapedata.grouped_nodes.shift();
-  }
-
-  var shapes = shapedata.shapes,
-  grouped_nodes = shapedata.grouped_nodes;
-
-  // console.log("start", shapes)
-
-
-  pinned_shapes = []
-
-  // update positions of all shapes
-  // apply momentum damping
-  // update center of mass
-  var x_center_of_mass = 0, y_center_of_mass = 0;
-  for (var i = 0; i < shapes.length; i++) {
-    // console.log(shapes[i])
-    // if (!shapes[i].pinned)
-    if (shapes[i].pinned) {
-      pinned_shapes.push(shapes[i])
+    // console.log(shapedata)
+    if (typeof shapedata.grouped_nodes[0] === 'undefined') { // Find out why there is an empty
+      shapedata.grouped_nodes.shift();
     }
-    this._update_coords(shapes[i]);
-    this._damp_p(shapes[i]);
-    x_center_of_mass += !shapes[i].pinned ? shapes[i].pos.x : 0;
-    y_center_of_mass += !shapes[i].pinned ? shapes[i].pos.y : 0;
-  }
 
-  // follow center of mass
-  shapes.forEach(function(n) {
-    n.pos.x -= !n.pinned ? x_center_of_mass / shapes.length : 0;
-    n.pos.y -= !n.pinned ? y_center_of_mass / shapes.length : 0;
-  });
+    var shapes = shapedata.shapes,
+        grouped_nodes = shapedata.grouped_nodes;
 
-  // apply spring forces by grouping of nodes by color
-  for (i = 0; i < grouped_nodes.length; i++) {
-    this._apply_colored_spring(grouped_nodes[i], shapes);
-  }
 
-  // apply collision forces to each pair of shapes
-  for (i = 0; i < shapes.length; i++) {
-    var s = shapes[i];
-    for (var j = 0; j < i; j++) {
-      var s2 = shapes[j];
-      this._apply_collision(s, s2);
+    pinned_shapes = []
+
+    // update positions of all shapes
+    // apply momentum damping
+    // update center of mass
+    var x_center_of_mass = 0, y_center_of_mass = 0;
+    for (var i = 0; i < shapes.length; i++) {
+        // console.log(shapes[i])
+        // if (!shapes[i].pinned)
+        if (shapes[i].pinned) {
+          pinned_shapes.push(shapes[i])
+        }
+        this._update_coords(shapes[i]);
+        this._damp_p(shapes[i]);
+        x_center_of_mass += !shapes[i].pinned ? shapes[i].pos.x : 0;
+        y_center_of_mass += !shapes[i].pinned ? shapes[i].pos.y : 0;
     }
-  }
 
-  ctr = 0
-  for (i in shapes) {
-    if (shapes[i].pinned) {
-      // console.log("before", shapes[i])
-      shapes[i] = pinned_shapes[ctr]
-      // console.log(pinned_shapes[ctr])
-      // console.log("after", shapes[i])
-      ctr += 1
+    // follow center of mass
+    shapes.forEach(function(n) {
+      n.pos.x -= !n.pinned ? x_center_of_mass / shapes.length : 0;
+      n.pos.y -= !n.pinned ? y_center_of_mass / shapes.length : 0;
+    });
+
+    // apply spring forces by grouping of nodes by color
+    for (i = 0; i < grouped_nodes.length; i++) {
+        this._apply_colored_spring(grouped_nodes[i], shapes);
     }
-  }
-  // console.log(shapes)
-  // debugger;
+
+    // apply collision forces to each pair of shapes
+    for (i = 0; i < shapes.length; i++) {
+        var s = shapes[i];
+        for (var j = 0; j < i; j++) {
+            var s2 = shapes[j];
+            this._apply_collision(s, s2);
+        }
+    }
+
+    ctr = 0
+    for (i in shapes) {
+      if (shapes[i].pinned) {
+        // console.log("before", shapes[i])
+        shapes[i] = pinned_shapes[ctr]
+        // console.log(pinned_shapes[ctr])
+        // console.log("after", shapes[i])
+        ctr += 1
+      }
+    }
+    // console.log(shapes)
+    // debugger;
 };
 
 ////////////////////////////////////////////////////////////////////////////////
 
 Simulation.prototype._update_coords = function(s) {
-  /*
-  * update coords using last momenta
-  *
-  * params:
-  *   dt
-  *   angle_res
-  */
+    /*
+     * update coords using last momenta
+     *
+     * params:
+     *   dt
+     *   angle_res
+     */
 
-  var dt = this.dt,
-  angle_res = this.angle_res;
+    var dt = this.dt,
+        angle_res = this.angle_res;
 
-  // a) update position of shape's center
-  s.pos.x += !s.pinned ? s.lin_p.x / s.m * dt : 0; // x
-  s.pos.y += !s.pinned ? s.lin_p.y / s.m * dt : 0; // y
+    // a) update position of shape's center
+    s.pos.x += !s.pinned ? s.lin_p.x / s.m * dt : 0; // x
+    s.pos.y += !s.pinned ? s.lin_p.y / s.m * dt : 0; // y
 
-  // b) update and discretize orientation (angle)
-  if (!s.pinned){
-    s.setAngle(
-      // setAngle updates positions of vertices as s.calcPoints
-      Math.floor((
-        s.angle + (s.rot_p / s.I * dt)
-      ) / angle_res) * angle_res
+    // b) update and discretize orientation (angle)
+    if (!s.pinned){
+      s.setAngle(
+          // setAngle updates positions of vertices as s.calcPoints
+          Math.floor((
+              s.angle + (s.rot_p / s.I * dt)
+          ) / angle_res) * angle_res
 
-    );
-  }
+      );
+    }
 
-  var cos = Math.cos(s.angle);
-  var sin = Math.sin(s.angle);
+    var cos = Math.cos(s.angle);
+    var sin = Math.sin(s.angle);
 
-  // c) update positions of nodes with new position and orientation
-  s.nodes.forEach(function(n) {
-    n.dx = !s.pinned ? cos * n.ax - sin * n.ay : 0;
-    n.dy = !s.pinned ? sin * n.ax + cos * n.ay : 0;
-    n.x = !s.pinned ? s.pos.x + n.dx : 0;
-    n.y = !s.pinned ? s.pos.y + n.dy : 0;
-  });
+    // c) update positions of nodes with new position and orientation
+    s.nodes.forEach(function(n) {
+      n.dx = !s.pinned ? cos * n.ax - sin * n.ay : 0;
+      n.dy = !s.pinned ? sin * n.ax + cos * n.ay : 0;
+      n.x = !s.pinned ? s.pos.x + n.dx : 0;
+      n.y = !s.pinned ? s.pos.y + n.dy : 0;
+    });
 };
 
 Simulation.prototype._damp_p = function(s) {
-  /*
-  * apply momentum damping to shape s
-  *
-  * params:
-  *   linp_damping
-  *   rotp_damping
-  */
-  s.lin_p.x *= this.linp_damping;
-  s.lin_p.y *= this.linp_damping;
-  s.rot_p *= this.rotp_damping;
-  // s.lin_p.x *= !s.pinned ? this.linp_damping : 0;
-  // s.lin_p.y *= !s.pinned ? this.linp_damping : 0;
-  // s.rot_p *= !s.pinned ? this.rotp_damping : 0;
+    /*
+     * apply momentum damping to shape s
+     *
+     * params:
+     *   linp_damping
+     *   rotp_damping
+     */
+    s.lin_p.x *= !s.pinned ? this.linp_damping : 0;
+    s.lin_p.y *= !s.pinned ? this.linp_damping : 0;
+    s.rot_p *= !s.pinned ? this.rotp_damping : 0;
 };
 
 Simulation.prototype._apply_colored_spring = function(node_group, shapes) {
-  /*
-  * p += f(x)
-  *
-  * equations of motion are second-order.
-  * calculate forces using new positions, and update momenta
-  * by applying spring-interactions between node_group
-  *
-  * nodes_group: list of nodes which all attract to each other
-  *                    each node contains a reference to its host shape
-  *                    as an index in the list `shapes`
-  *
-  * shapes: list of shapes which contain the nodes, in order
-  *         according to references maintained by node_group
-  *
-  * params:
-  *   dt
-  *   hooke
-  */
+    /*
+     * p += f(x)
+     *
+     * equations of motion are second-order.
+     * calculate forces using new positions, and update momenta
+     * by applying spring-interactions between node_group
+     *
+     * nodes_group: list of nodes which all attract to each other
+     *                    each node contains a reference to its host shape
+     *                    as an index in the list `shapes`
+     *
+     * shapes: list of shapes which contain the nodes, in order
+     *         according to references maintained by node_group
+     *
+     * params:
+     *   dt
+     *   hooke
+     */
 
-  // console.log(node_group)
-  var dt = this.dt,
-  hooke = this.hooke / node_group.length;
+    var dt = this.dt,
+        hooke = this.hooke / node_group.length;
 
-  var n, n2, s, s2, k;
-  for (var j = 0; j < node_group.length; j++) {
-    n = node_group[j];
-    s = shapes[n.s_index];
+    var n, n2, s, s2, k;
+    for (var j = 0; j < node_group.length; j++) {
+        n = node_group[j];
+        s = shapes[n.s_index];
 
-    // each pair of like-colored nodes (j, k)
-    for (k = 0; k < j; k++) {
-      n2 = node_group[k];
-      s2 = shapes[n2.s_index];
-      // linear forces applied to both shapes
-      s.lin_p.x += hooke * (n2.x - n.x) * dt;
-      s.lin_p.y += hooke * (n2.y - n.y) * dt;
-      s2.lin_p.x += hooke * (n.x - n2.x) * dt;
-      s2.lin_p.y += hooke * (n.y - n2.y) * dt;
-      // s.lin_p.x += !s.pinned ? hooke * (n2.x - n.x) * dt : 0;
-      // s.lin_p.y += !s.pinned ? hooke * (n2.y - n.y) * dt : 0;
-      // s2.lin_p.x += !s2.pinned ? hooke * (n.x - n2.x) * dt : 0;
-      // s2.lin_p.y += !s2.pinned ? hooke * (n.y - n2.y) * dt : 0;
+        // each pair of like-colored nodes (j, k)
+        for (k = 0; k < j; k++) {
+            n2 = node_group[k];
+            s2 = shapes[n2.s_index];
+            // linear forces applied to both shapes
+            s.lin_p.x += hooke * (n2.x - n.x) * dt;
+            s.lin_p.y += hooke * (n2.y - n.y) * dt;
+            s2.lin_p.x += hooke * (n.x - n2.x) * dt;
+            s2.lin_p.y += hooke * (n.y - n2.y) * dt;
+            // s.lin_p.x += !s.pinned ? hooke * (n2.x - n.x) * dt : 0;
+            // s.lin_p.y += !s.pinned ? hooke * (n2.y - n.y) * dt : 0;
+            // s2.lin_p.x += !s2.pinned ? hooke * (n.x - n2.x) * dt : 0;
+            // s2.lin_p.y += !s2.pinned ? hooke * (n.y - n2.y) * dt : 0;
 
-      // torques applied to both shapes
-      s.rot_p += !s.pinned ? hooke * (
-        n.dx * (n2.y - n.y) -
-        n.dy * (n2.x - n.x)
-      ) * dt : 0;
-      s2.rot_p += !s2.pinned ? hooke * (
-        n2.dx * (n.y - n2.y) -
-        n2.dy * (n.x - n2.x)
-      ) * dt : 0;
+            // torques applied to both shapes
+            s.rot_p += !s.pinned ? hooke * (
+                n.dx * (n2.y - n.y) -
+                n.dy * (n2.x - n.x)
+            ) * dt : 0;
+            s2.rot_p += !s2.pinned ? hooke * (
+                n2.dx * (n.y - n2.y) -
+                n2.dy * (n.x - n2.x)
+            ) * dt : 0;
+        }
     }
-  }
 };
 
 Simulation.prototype._apply_collision = function(s, s2) {
-  /*
-  * apply collision forces to pair of shapes (updates p)
-  *
-  * params:
-  *   dt
-  *   solid_hooke
-  */
+    /*
+     * apply collision forces to pair of shapes (updates p)
+     *
+     * params:
+     *   dt
+     *   solid_hooke
+     */
 
-  var repulsion = this.dt * this.solid_hooke;
+    var repulsion = this.dt * this.solid_hooke;
 
-  var overlap_x = 0,
-  overlap_y = 0;
+    var overlap_x = 0,
+        overlap_y = 0;
 
-  this.response.clear();
-  if (SAT.testPolygonPolygon(s, s2, this.response)) {
-    overlap_x -= this.response.overlapV.x;
-    overlap_y -= this.response.overlapV.y;
-  }
-  this.response.clear();
-  if (SAT.testPolygonPolygon(s2, s, this.response)) {
-    overlap_x += this.response.overlapV.x;
-    overlap_y += this.response.overlapV.y;
-  }
+    this.response.clear();
+    if (SAT.testPolygonPolygon(s, s2, this.response)) {
+        overlap_x -= this.response.overlapV.x;
+        overlap_y -= this.response.overlapV.y;
+    }
+    this.response.clear();
+    if (SAT.testPolygonPolygon(s2, s, this.response)) {
+        overlap_x += this.response.overlapV.x;
+        overlap_y += this.response.overlapV.y;
+    }
 
-  if (overlap_x || overlap_y) {
+    if (overlap_x || overlap_y) {
 
-    // conserves linear momentum and applies
-    // a psuedo-hookean force (linear-elastic)
-    s.lin_p.x += overlap_x * repulsion;
-    s.lin_p.y += overlap_y * repulsion;
-    s2.lin_p.x -= overlap_x * repulsion;
-    s2.lin_p.y -= overlap_y * repulsion;
-    // s.lin_p.x += !s.pinned ? overlap_x * repulsion : 0;
-    // s.lin_p.y += !s.pinned ? overlap_y * repulsion : 0;
-    // s2.lin_p.x -= !s2.pinned ? overlap_x * repulsion : 0;
-    // s2.lin_p.y -= !s2.pinned ? overlap_y * repulsion : 0;
-  }
+        // conserves linear momentum and applies
+        // a psuedo-hookean force (linear-elastic)
+        s.lin_p.x += !s.pinned ? overlap_x * repulsion : 0;
+        s.lin_p.y += !s.pinned ? overlap_y * repulsion : 0;
+        s2.lin_p.x -= !s2.pinned ? overlap_x * repulsion : 0;
+        s2.lin_p.y -= !s2.pinned ? overlap_y * repulsion : 0;
+    }
 };
